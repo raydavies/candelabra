589d164026895b3e2e4d4519e594796f3c710459
Jordan <jordanmand@gmail.com>
2014-12-10 01:05:22 -0600

    Update config

---
+++ .env.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	'db_host'		=> 'localhost',
+	'db_database'	=> 'mumbles_trip',
+	'db_username'	=> 'mumbles_jordan',
+	'db_password'	=> 'test',
+);
--- db.env.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	'db_host'		=> 'localhost',
-	'db_database'	=> 'candelabra',
-	'db_username'	=> 'janderson',
-	'db_password'	=> 'e4g135ss'
-);
